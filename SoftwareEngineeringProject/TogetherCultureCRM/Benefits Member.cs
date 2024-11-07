7ac91270c46d085e7abfeab8401c2a73f822ada1
SamMGuni <sm2738@student.aru.ac.uk>
2024-11-07 14:37:35 +0000

    adding sam work

---
+++ SoftwareEngineeringProject/TogetherCultureCRM/Benefits Member.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engineering_Project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
