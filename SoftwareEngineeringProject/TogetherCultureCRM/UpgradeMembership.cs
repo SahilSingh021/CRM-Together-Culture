1699f2f6749dfd78adc4b16c8a0dcca7459e6b09
Kai-02 <ka900@student.aru.ac.uk>
2024-11-07 13:57:01 +0000

    form rename process

---
+++ SoftwareEngineeringProject/TogetherCultureCRM/UpgradeMembership.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TogetherCultureCRM
+{
+    public partial class UpgradeMembership : Form
+    {
+        public UpgradeMembership()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
