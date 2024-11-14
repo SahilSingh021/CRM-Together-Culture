03a3090463a28895a962c9f0037877a4486aeb49
SamMGuni <sm2738@student.aru.ac.uk>
2024-11-14 16:14:12 +0000

    adding buttons

--- SoftwareEngineeringProject/TogetherCultureCRM/Program.cs
+++ SoftwareEngineeringProject/TogetherCultureCRM/Program.cs
@@ -17,6 +17,7 @@ namespace TogetherCultureCRM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new PlaceHire());
+
         }
     }
 }
