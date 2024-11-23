b62ddaabde37dc458a943aac80bda56a13eb989f
Kai-02 <ka900@student.aru.ac.uk>
2024-11-23 15:08:59 +0000

    adding member homepage

---
+++ TogetherCultureCRM/TogetherCultureCRM/Classes/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TogetherCultureCRM.Classes
+{
+    internal class Member
+    {
+        public int memberId {  get; set; }
+        public int userId { get; set; }
+    }
+}
